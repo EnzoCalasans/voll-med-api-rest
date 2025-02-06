12a4449d6fd30cbfdd8c5888bb4eac25c349d706
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2025-02-06 15:26:53 -0300

    spring data jpa

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,34 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    String logradouro;
+    String bairro;
+    String cep;
+    String cidade;
+    String uf;
+    String complemento;
+    String numero;
+
+    public Endereco(){
+
+    }
+
+    public Endereco(DadosEndereco dados){
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
+}
---
+++ src/main/resources/db/migration/V2__alter-table-medicos-add-column-telefone.sql
@@ -0,0 +1,1 @@
+alter table medicos ad telefone varchar(20) not null;
