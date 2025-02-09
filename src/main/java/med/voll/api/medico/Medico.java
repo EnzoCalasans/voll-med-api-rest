7e3c241b9cd34cb2c022444ee19d25480b147a39
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2025-02-09 14:51:53 -0300

    requisições put e delete

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,6 +1,7 @@
 package med.voll.api.medico;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,6 +30,9 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    @Column(insertable = false)
+    private boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -36,5 +40,22 @@ public class Medico {
         this.crm = dados.crm();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformações(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformações(dados.endereco());
+        }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
