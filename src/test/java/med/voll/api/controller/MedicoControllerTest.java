0dbe9939395072a1207863bda5a4229480baf138
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2025-02-11 17:09:06 -0300

    test: Adicionando testes e remocao de dependencias depreciadas no pom

--- src/test/java/med/voll/api/controller/MedicoControllerTest.java
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +9,6 @@ class MedicoControllerTest {
 
     @Test
     void cadastrar() {
+        Assertions.assertEquals(0,0);
     }
 }
