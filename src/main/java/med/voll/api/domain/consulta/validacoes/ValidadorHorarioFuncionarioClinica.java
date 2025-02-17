105491047a07045c932b426f12e5aa18be40ae89
Enzo <Enzocalasanscardosocorreacosta@gmail.com>
2025-02-17 17:15:40 -0300

    Testando o agendamento

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionarioClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionarioClinica.java
@@ -12,7 +12,7 @@ public class ValidadorHorarioFuncionarioClinica implements ValidadorAgendamentoD
         var dataConsulta = dados.data();
         var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
         var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
-        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() < 18;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
         if(domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica){
             throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica!");
         }
